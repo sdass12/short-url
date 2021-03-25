ce2f94d40fb1b9504e2f40feb8fb5d69b4bdd805
sdass12 <hc09471@gmail.com>
2021-03-25 16:36:36 +0900

    Modify : modify Hashing

--- src/short-url/short-url.service.ts
+++ src/short-url/short-url.service.ts
@@ -8,46 +8,36 @@ export class ShortUrlService {
     constructor(
         @InjectRepository(ShortUrlEntity)
         private readonly shortUrlRepository: Repository<ShortUrlEntity>
-    ) {
-    }
+    ) {}
 
 
     getRealUrl(url: string) {
-        return this.shortUrlRepository.findOne({url_short:url});
+        return "test";//this.shortUrlRepository.findOne({url_short:url});
     }
 
     getAll() {
         return this.shortUrlRepository.find();
     }
 
-    create(url: string) {
-        function changeUrl(url) {
-            let changeUrl = "http://localhost:3000/";
-            var hash = 0, i, chr;
-            if (url.length === 0) return hash;
-            for (i = 0; i < url.length; i++) {
-                chr = url.charCodeAt(i);
-                hash = ((hash << 5) - hash) + chr;
-                hash |= 0; // Convert to 32bit integer
-            }
-            changeUrl += hash.toString(16);
-
-            return changeUrl;
-        };
-
+     create(url: string) {
         const shortUrl = require('node-url-shortener');
 
         const saveUrl = async () => {
-            const changedUrl = shortUrl.short(url, function(err, url){return url;});
+             let changedUrl = new Promise(((resolve, reject) => {
+                shortUrl.short(url, function(err, shortenerUrl){
+                     resolve(shortenerUrl.replace(/https:\/\/.*\//g,''));
+                });
+            }))
 
             const isThereRow = await this.shortUrlRepository.findOne({url_real: url})
             if (isThereRow == undefined) {
                 // @ts-ignore
                 await this.shortUrlRepository.save({
                     url_real: url,
-                    url_short: changedUrl
+                    url_short: await changedUrl
                 })
             }
+
             return changedUrl
         }
 
