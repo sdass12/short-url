6d100182977bc15dbc2e903a9ee5446df88be95e
sdass <45581595+sdass12@users.noreply.github.com>
2021-06-02 09:52:24 +0900

    Modify : reduce Max size of hash column

--- src/short-url/entity/short-url.entity.ts
+++ src/short-url/entity/short-url.entity.ts
@@ -8,6 +8,6 @@ export class ShortUrlEntity {
     @Column({unique: true, length: 255})
     url_real: string;
 
-    @Column({unique: true, length: 50})
+    @Column({unique: true, length: 8})
     url_short: string;
 }
